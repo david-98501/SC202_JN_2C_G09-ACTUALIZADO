b08bdb89634964ac4ada7ccdc6f1193877013a73
David Madrigal <dm5566694@gmail.com>
2025-08-04 20:31:38 -0600

    se trabajo la clase productos

--- ProyectoJava/src/main/java/com/mycompany/proyectojava/Productos.java
+++ ProyectoJava/src/main/java/com/mycompany/proyectojava/Productos.java
@@ -4,10 +4,54 @@
  */
 package com.mycompany.proyectojava;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Dell
  */
 public class Productos {
+    
+    public static int[] sumaProductos;
+
+    public Productos(int cantidad) {
+        sumaProductos = new int[cantidad];
+    }
+
+    public void escogerDia(int cliente) {
+        String mensaje = "Paquetes disponibles:\n" +
+                "1. Semana completa (50000)\n" +
+                "2. 3 dias y 2 noches (70000)\n" +
+                "3. VIP (10000)\n" +
+                "4. Todo incluido + recreativas (120000)";
+        JOptionPane.showMessageDialog(null, mensaje);
+
+        int opcion = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero del paquete deseado"));
+
+        switch (opcion) {
+            case 1: sumaProductos[cliente] += Facturacion.semanaCompleta; break;
+            case 2: sumaProductos[cliente] += Facturacion.dosNoches; break;
+            case 3: sumaProductos[cliente] += Facturacion.vip; break;
+            case 4: sumaProductos[cliente] += Facturacion.recreativas; break;
+            default: JOptionPane.showMessageDialog(null, "Seleccion incorrecta.");
+        }
+    }
+
+    public void paquetesAdicionales(int cliente) {
+        String mensaje = "Paquetes adicionales:\n1. Camping (10000)\n2. Senderismo (25000)";
+        JOptionPane.showMessageDialog(null, mensaje);
+
+        int seleccion = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la opcion deseada"));
+
+        if (seleccion == 1) {
+            sumaProductos[cliente] += Facturacion.camping;
+            JOptionPane.showMessageDialog(null, "Camping agregado.");
+        } else if (seleccion == 2) {
+            sumaProductos[cliente] += Facturacion.senderismo;
+            JOptionPane.showMessageDialog(null, "Senderismo agregado.");
+        } else {
+            JOptionPane.showMessageDialog(null, "No se agregaron adicionales.");
+        }
+    }
     
 }
